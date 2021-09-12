7dd478bb9e0c92f7249071d4352d140ee24ee761
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-09-12 20:57:05 +0430

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useFetch } from "./useFetch";
 import Follower from "./Follower";
+import "./styles.css";
 function App() {
   const { loading, data } = useFetch();
   const [page, setPage] = useState(0);
